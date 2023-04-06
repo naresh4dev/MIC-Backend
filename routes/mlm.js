ebef30ab6a03f94941b4f38a20857da266ab7772
Naresh <naresh45.dev@gmail.com>
2023-04-06 16:45:59 +0530

    update mlm tree for New Member json

--- routes/mlm.js
+++ routes/mlm.js
@@ -4,9 +4,6 @@ const { route } = require('./auth');
 const router = require('express').Router();
 
 
-
-
-
 router.get('/',(req,res)=>{
     try {
         const request = req.app.locals.db.request();
@@ -14,7 +11,6 @@ router.get('/',(req,res)=>{
         const query = `select MemberID, ParentID, LeftChildID, RightChildID from BinaryTreeMLM ; `
         request.query(query,(queryErr,result)=>{
             if(!queryErr) {
-
                 const json = {tree : result.recordset};
                 let orgChartJson = {};
 
@@ -23,7 +19,7 @@ router.get('/',(req,res)=>{
 
                 // Create the root node in org chart format
                 orgChartJson.id = rootNode.MemberID;
-
+                orgChartJson.children = [];
                 // Recursively add children to the root node
                 addChildrenToNode(orgChartJson, rootNode, json.tree);
 
@@ -33,13 +29,21 @@ router.get('/',(req,res)=>{
 
                 if (children.length > 0) {
                     // Create an empty children array for the org chart node
-                    orgChartNode.children = [];
-
+                    
+                    console.log(orgChartNode);
                     // Loop through the children and add them to the org chart node
                     children.forEach(child => {
-                    let childNode = { id: child.MemberID };
-                    orgChartNode.children.push(childNode);
 
+                    let   childNode = { id: child.MemberID, parent_id : child.ParentID, left_child_id : child.LeftChildID, right_child_id : child.RightChildID, children :[]  };
+                        if(child.LeftChildID==null && child.RightChildID==null) {
+                            childNode['children'].push({id:'L', title : "New Order", name :"Click Here" ,parentID : child.MemberID});
+                            childNode['children'].push({id:'R',title : "New Order", name :"Click Here" ,parentID:child.MemberID});
+                        } else if (child.LeftChildID==null || child.RightChildID==null) {
+                            const position = child.LeftChildID?'R' :'L';
+                            childNode['children'].push({id:position,title : "New Order", name :"Click Here" ,parentID:child.MemberID}); 
+                            
+                        }
+                        orgChartNode['children'].push(childNode);
                     // Recursively add children to the child node
                     addChildrenToNode(childNode, child, tree);
                     });
