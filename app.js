0a442bcf704b1dfeee6e1c201a2dfc514f158f11
Naresh <naresh45.dev@gmail.com>
2023-04-20 17:10:35 +0530

    resolve cookie issue

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ app.use(session({
     secret :process.env.SESSION_SECRET,
     cookie : {
         maxAge: 24 * 60 * 60 * 1000,
-        secure : true
+        resave: false,
+        saveUninitialized: false,
     },
     resave : true,
 }));
